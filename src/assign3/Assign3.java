ecfcf2a21932a23afb29aa657b0fa10a310eedd6
LynnethTigsePena4240 <lynneth_tigsepena@hotmail.com>
2025-11-09 19:03:13 -0500

    working on reading the file

--- src/assign3/Assign3.java
+++ src/assign3/Assign3.java
@@ -8,13 +8,37 @@ package assign3;
  *
  * @author lynne
  */
+import java.io.*;
+import java.util.*;
 public class Assign3 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        //creating two maps
+        Map<String, Integer> customers = new HashMap<>(); 
+        Map<String, Integer> products = new HashMap<>(); 
+        
+        //created a file object to make the file to read from
+        File file1 = new File("src/assign3/Sales.txt"); 
+        
+        //reading the file
+        try(BufferedReader buff = new BufferedReader(new FileReader(file1)))
+        {
+            String line;
+            //read the file till the ends
+            while ((line = buff.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("file not found: " + e);
+        }
     }
     
 }
