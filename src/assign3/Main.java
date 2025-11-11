f86c75e244f51fc1d94e61a258cd6fbbd3f6055d
Annie Nguyen <a.nguyen2@hotmail.com>
2025-11-11 00:00:10 -0500

    added Main.java and placeholder classes

---
+++ src/assign3/Main.java
@@ -0,0 +1,19 @@
+package assignment3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Customer> customers = new HashMap<>();
+        Map<String, Product> products = new HashMap<>();
+        List<Sale> sales = new ArrayList<>();
+
+        System.out.println("Reading sales file...");
+
+        System.out.println("Inserting data...");
+
+        System.out.println("Updating records...");
+
+        System.out.println("All weekly processing complete.");
+    }
+}
