19579b1573690c48f82c05fae4e17ac8ebf7385e
Annie Nguyen <a.nguyen2@hotmail.com>
2025-11-13 23:58:44 -0500

    removed placeholders

--- src/assign3/Main.java
+++ src/assign3/Main.java
@@ -1,19 +1,6 @@
-package assignment3;
-
-import java.util.*;
-
+package assign3;
 public class Main {
     public static void main(String[] args) {
-        Map<String, Customer> customers = new HashMap<>();
-        Map<String, Product> products = new HashMap<>();
-        List<Sale> sales = new ArrayList<>();
-
-        System.out.println("Reading sales file...");
-
-        System.out.println("Inserting data...");
-
-        System.out.println("Updating records...");
-
-        System.out.println("All weekly processing complete.");
+        Assign3.main(args);
     }
 }
